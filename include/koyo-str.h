f22b02c89431a511e97770d20776359e504dce6b
osamikoyo <v.pylayev@mail.ru>
2025-01-16 17:57:14 +0500

    basic architecture

---
+++ include/koyo-str.h
@@ -0,0 +1,10 @@
+#ifdef FACEKID_H
+#define FACEKID_H
+
+char * to_upper(char * text);
+char * to_lower(char * text);
+int index(char s, char * text);
+char * trim_left(int index, char * text);
+char * trim_right(int index, char * text);
+
+#endif
